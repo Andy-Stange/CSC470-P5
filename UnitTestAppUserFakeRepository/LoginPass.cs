ea9f22b836eeb03296fd398c7c0a39122cc584bd
Michael Moore <Michael.Moore@trojans.dsu.edu>
2020-09-17 13:40:33 -0500

    Completed LoginPass check

--- UnitTestAppUserFakeRepository/LoginPass.cs
+++ UnitTestAppUserFakeRepository/LoginPass.cs
@@ -12,12 +12,16 @@ namespace UnitTestAppUserFakeRepository
         public void LoginWithCorrectCredentialsSucccess()
         {
             //arrange
+            const string UserName = "AndyStange17";
+            const string Password = "AS0717@!";
+            FakeAppUserRepository FakeUser = new FakeAppUserRepository();
 
             //act
 
-
+            Boolean Auth = FakeUser.Login(UserName, Password);
 
             //assert
+            Assert.IsTrue(Auth);
         }
     }
 }
