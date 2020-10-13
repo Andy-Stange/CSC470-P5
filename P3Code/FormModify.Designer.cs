56957978e4de50ed2cb832f1ed8efe486f60c418
Michael <Michael.Moore@trojans.dsu.edu>
2020-10-12 21:30:28 -0500

    Created forms and placeholders

---
+++ P3Code/FormModify.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace P3Code
+{
+    partial class FormModify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ModifyCancel = new System.Windows.Forms.Button();
+            this.ModifyAccept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ModifyCancel
+            // 
+            this.ModifyCancel.Location = new System.Drawing.Point(159, 114);
+            this.ModifyCancel.Name = "ModifyCancel";
+            this.ModifyCancel.Size = new System.Drawing.Size(75, 23);
+            this.ModifyCancel.TabIndex = 2;
+            this.ModifyCancel.Text = "Cancel";
+            this.ModifyCancel.UseVisualStyleBackColor = true;
+            // 
+            // ModifyAccept
+            // 
+            this.ModifyAccept.Location = new System.Drawing.Point(275, 114);
+            this.ModifyAccept.Name = "ModifyAccept";
+            this.ModifyAccept.Size = new System.Drawing.Size(75, 23);
+            this.ModifyAccept.TabIndex = 3;
+            this.ModifyAccept.Text = "Modify";
+            this.ModifyAccept.UseVisualStyleBackColor = true;
+            // 
+            // FormModify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 173);
+            this.Controls.Add(this.ModifyAccept);
+            this.Controls.Add(this.ModifyCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormModify";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modify Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ModifyCancel;
+        private System.Windows.Forms.Button ModifyAccept;
+    }
+}
---
+++ P3Code/FormRemove.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace P3Code
+{
+    partial class FormRemove
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RemoveCancel = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project to remove:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PlaceholderLabel";
+            // 
+            // RemoveCancel
+            // 
+            this.RemoveCancel.Location = new System.Drawing.Point(189, 130);
+            this.RemoveCancel.Name = "RemoveCancel";
+            this.RemoveCancel.Size = new System.Drawing.Size(75, 23);
+            this.RemoveCancel.TabIndex = 2;
+            this.RemoveCancel.Text = "Cancel";
+            this.RemoveCancel.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(343, 130);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(108, 23);
+            this.RemoveButton.TabIndex = 3;
+            this.RemoveButton.Text = "Remove Project";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // FormRemove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 192);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.RemoveCancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormRemove";
+            this.Text = "Remove Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button RemoveCancel;
+        private System.Windows.Forms.Button RemoveButton;
+    }
+}
